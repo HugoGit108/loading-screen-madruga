d1f8790914272a926c15ac424894aa7a53b6bc06
root <root@Debian.local>
2023-12-06 14:22:53 +0000

    First Commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,27 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "localhost:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Discord do Madruga RP , diverte-te", icon: "assets/media/icons/discord.png", link: "https://discord.gg/dinero"},
+    {name: "vips", label: "Vips", description: "VIPS [VAI SER UMA PONTE PARA O NOSSO WEBSITE]", icon: "assets/media/icons/tebex.png", link: "https://dinero-vip.tebex.io"},
+];
+
+Config.HideoverlayKeybind = 112
+Config.CustomBindText = "F1"; 
+
+// Staff list
+Config.Staff = [
+    {name: "Goncalo Rocha", description: "Preto", color: "#fff", image: "https://cdn.discordapp.com/attachments/945854078020370462/954042338857066566/Dinero_logo.gif"},
+    {name: "HugoM", description: "Founder (melhor que o preto)", color: "#0195FD", image: "https://cdn.discordapp.com/attachments/945854078020370462/954042338857066566/Dinero_logo.gif"},
+];
+
+// Categories
+Config.Categories = [
+    {label: "Redes sociais", default: true},
+    {label: "Staff Madruga", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
